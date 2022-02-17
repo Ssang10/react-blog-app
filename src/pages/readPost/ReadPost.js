23df5792a424a0a2087102da9cd5a6df6282e6db
sanggam10 <shresthasanggam@gmail.com>
2022-02-17 22:39:38 +0900

    minor JSx update

--- src/pages/readPost/ReadPost.js
+++ src/pages/readPost/ReadPost.js
@@ -14,7 +14,7 @@ const ReadPost = () => {
   return (
     <div key={readPost.id} className="post">
       <h2 className="post-title">{readPost.title}</h2>
-      <h4 className="post-author">@{readPost.author.name}</h4>
+      <h4 className="post-author">{readPost.author.name}</h4>
       <h4 className="post-date">{postDate(readPost.date.fullDate)}</h4>
       <p className="post-text">{readPost.postText}</p>
     </div>
